4741db0619d85c7cd87066259604994b9f2ebc14
Dominik <dominik.hasso@gmail.com>
2019-04-01 16:21:35 +0200

    Php novínka

---
+++ PHP/resources/views/layouts/main.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="/css/cviko32.css">
+</head>
+
+<body>
+
+  <div class="container">
+    <header>
+      <div class="row h-100 bg-primary">
+        <div class="col align-self-center text-white" id="hlavicka">
+          <p class="font-weight-bold text-white">YEA BOY</p>
+        </div>
+      </div>
+    </header>
+
+    <div class="row">
+      <div class="col align-self-center m-md-2">
+        <ul class="nav nav-pills nav-fill flex-column flex-md-row ">
+          <li class="nav-item">
+            <a class="nav-link active" href="/">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="/Site1">Site1</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/Site2">Site2</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+  <div class="row">
+    <div class="col-md-3">
+      <div class="list-group">
+        <a href="#" class="list-group-item list-group-item-action active">
+          Cras justo odio
+        </a>
+        <a href="#" class="list-group-item list-group-item-action">Suprised Pikachu<span class="badge badge-secondary">New</span></a>
+        <a href="#" class="list-group-item list-group-item-action">It´s something</a>
+        <a href="#" class="list-group-item list-group-item-action">What if i told you</a>
+        <a href="#" class="list-group-item list-group-item-action">Confused Screaming</a>
+      </div>
+    </div>
+    <div class="col-md-9">
+      @yield("content")
+
+</div>
+      </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <p class="font-weight-bold text-dark" ><h3>Spread</h3>
+          <p>Najlepšia vlastonosť na PIKACHU je to že je to žltá haveť :D </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="card-footer text-muted">
+    @haššiš
+  </div>
+  <div class="row">
+  </div>
+    </div>
+
+
+</body>
+
+</html>
---
+++ PHP/resources/views/responsivita.blade.php
@@ -0,0 +1,27 @@
+@extends("layouts.main")
+        @section("content")
+          <div class="row">
+            <div class="col">
+              <p class="font-weight-bold text-dark" >Shinobi {{ $name }}</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-4">
+              <p class="font-weight-bold text-dark" ><h3>Die bitch</h3>
+              <p>let´s go</p>
+            </div>
+            <div class="col-md-4">
+              <p class="font-weight-bold text-dark" ><h3>HI my friend</h3>
+              <p>welcome to my jungle</p>
+            </div>
+            <div class="col-md-4">
+              <div class="card" style="width: 18rem;">
+                <img src="/JPG/index.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Rainbow 6 siege.</p>
+                  <a href="#" class="btn btn-primary">You must Download</a>
+                </div>
+              </div>
+            </div>
+@endsection
